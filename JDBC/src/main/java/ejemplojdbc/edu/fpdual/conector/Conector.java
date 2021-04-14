41c0b0c7c65fd715fd3498985eb6f82db3095061
Miijeh <danimijtut@gmail.com>
2021-04-14 14:55:33 +0200

    Ejemplos JDBC

--- JDBC/src/main/java/ejemplojdbc/edu/fpdual/conector/Conector.java
+++ JDBC/src/main/java/ejemplojdbc/edu/fpdual/conector/Conector.java
@@ -1,18 +1,52 @@
 package ejemplojdbc.edu.fpdual.conector;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class Conector {
 
 	Properties prop = new Properties();
-	
+
 	public Conector() {
 		try {
 			prop.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
-		}catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	public Connection getMySQLConnection() {
+		try {
+
+			// Indicates which driver is going to be used.
+			Class.forName(prop.getProperty(MySQLConstants.DRIVER));
+			// Class.forName("com.mysql.cj.jdbc.Driver");
+			try {
+				// Creates the connection based on the obtained URL.
+				return DriverManager.getConnection(getURL());
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private String getURL() {
+		return new StringBuilder().append(prop.getProperty(MySQLConstants.URL_PREFIX))
+				.append(prop.getProperty(MySQLConstants.URL_HOST)).append(":")
+				.append(prop.getProperty(MySQLConstants.URL_PORT)).append("/")
+				.append(prop.getProperty(MySQLConstants.URL_SCHEMA)).append("?user=")
+				.append(prop.getProperty(MySQLConstants.USER)).append("&password=")
+				.append(prop.getProperty(MySQLConstants.PASSWD)).append("&useSSL=")
+				.append(prop.getProperty(MySQLConstants.URL_SSL)).toString();
+	}
+	// return
+	// "jdbc:mysql://localhost:3306/world?user=sa&password=12345678&userSSL=false";
+
 }
---
+++ JDBC/src/main/java/ejemplojdbc/edu/fpdual/main/MainEjemploJDBC.java
@@ -0,0 +1,22 @@
+package ejemplojdbc.edu.fpdual.main;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import ejemplojdbc.edu.fpdual.conector.Conector;
+
+public class MainEjemploJDBC {
+
+	public static void main(String[] args) {
+		// Connects to the DB
+		try (Connection con = new Conector().getMySQLConnection()) {
+			// Looks for all the cities in the DB and prints them.
+			// new CityManager().findAll(con).forEach(city ->System.out.println(city));
+			System.out.println(con.getCatalog());
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
